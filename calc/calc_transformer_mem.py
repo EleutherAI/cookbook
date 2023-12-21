da43b62199e0ed778b7c9c78d8d62f9544bcb02b
Hailey Schoelkopf <65563625+haileyschoelkopf@users.noreply.github.com>
2023-12-21 16:20:43 -0500

    Draft: Add GQA/MQA memory calcs (assumes repeat_interleave)

--- calc/calc_transformer_mem.py
+++ calc/calc_transformer_mem.py
@@ -54,6 +54,10 @@ def config_parser():
                         type=int,
                         default=64,
                         help='Number of attention heads used in model')
+    parser.add_argument("--kv-head-ratio", "-kv",
+                        type=float,
+                        default=1.0,
+                        help='Ratio of total query heads to key/value heads. 1.0 for MHA, 1/num_attention_heads for MQA.')
     parser.add_argument("--sequence-length", "-s",
                         type=int,
                         default=2048,
@@ -114,7 +118,7 @@ def calc_mem(args):
     embed_params = 2 * args.vocab_size * args.hidden_size
     positional_params = args.hidden_size * args.sequence_length
     ln_params = 8 * args.hidden_size * args.num_layers + (2 * args.hidden_size)
-    attention_params = 4 * args.num_layers * args.hidden_size * args.hidden_size
+    attention_params = 2 * (1 + int(args.num_attention_heads * args.kv_head_ratio)) * args.num_layers * args.hidden_size * args.hidden_size
     mlp_params = 2 * args.num_layers * args.hidden_size * args.ffn_expansion_factor * args.hidden_size
     total_params = embed_params + positional_params + ln_params + attention_params + mlp_params
     if args.num_experts > 0:
