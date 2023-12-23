8891d5e87c6030458842b858947499f3f10c49d6
Quentin Anthony <qganthony@yahoo.com>
2023-12-23 14:51:45 -0800

    Fix default backend to no longer rely on DS

--- benchmarks/communication/constants.py
+++ benchmarks/communication/constants.py
@@ -3,7 +3,7 @@ from deepspeed.accelerator import get_accelerator
 DEFAULT_WARMUPS = 5
 DEFAULT_TRIALS = 50
 DEFAULT_TYPE = 'float'
-DEFAULT_BACKEND = get_accelerator().communication_backend_name()
+DEFAULT_BACKEND = 'nccl'
 DEFAULT_UNIT = 'Gbps'
 DEFAULT_DIST = 'deepspeed'
 DEFAULT_MAXSIZE = 24
