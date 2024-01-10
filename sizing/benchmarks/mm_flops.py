d84395b38933f9cb576466aeb4d8de3a0b9f7ec5
jahatef <hatef.4@buckeyemail.osu.edu>
2024-01-10 20:59:44 +0000

    mm file

--- sizing/benchmarks/mm_flops.py
+++ sizing/benchmarks/mm_flops.py
@@ -1,67 +1,51 @@
 import time
 import torch
 import sys
+import numpy as np
+import argparse
 
-
-def benchmark_mm(m, n, k, num_iterations=100):
-    A = torch.randn(m, n).half().to("cuda:0")
-    B = torch.randn(n, k).half().to("cuda:0")
-    C = torch.empty(m, k).half().to("cuda:0")
-    num_warmup_iterations = 50
-    for i in range(num_warmup_iterations + num_iterations):
-        if i == num_warmup_iterations:
-            start_time = time.time()
-        with torch.no_grad():
-            torch.mm(A, B, out=C)
-        torch.cuda.synchronize()
-    elapsed_time = (time.time() - start_time) / num_iterations
-    print(f"Elapsed time for {m}x{n}x{k}: {elapsed_time:.3f}")
-    print(f"Throughput (in TFLOP/s) for {m}x{n}x{k}: {(2 * m * n * k) / (elapsed_time * 10**12):.3f}")
-    print("-" * 80)
-
-def benchmark_mm_b(m, n, k, b=None, num_iterations=100):
-    
-    B = torch.randn(k, n).half().to("cuda:0")
-    if b is None:
-        A = torch.randn(m, n).half().to("cuda:0")
-        b=1
-        C = torch.empty(m, k).half().to("cuda:0")
-    else:
-        A = torch.randn(b,m,n).half().to("cuda:0")
-        C = torch.empty(b,m, k).half().to("cuda:0")
-    num_warmup_iterations = 50
-    for i in range(num_warmup_iterations + num_iterations):
-        if i == num_warmup_iterations:
-            start_time = time.time()
-        with torch.no_grad():
-            torch.nn.functional.linear(A, B, out=C)
-        torch.cuda.synchronize()
-    elapsed_time = (time.time() - start_time) / num_iterations
-    if b is None:
-        print(f"Elapsed time for {m}x{n}x{k}: {elapsed_time:.3f}")
-        print(f"Throughput (in TFLOP/s) for {m}x{n}x{k}: {(2 * m * n * k) / (elapsed_time * 10**12):.3f}")
-    else:
-        print(f"Elapsed time for {m}x{n}x{k}, b={b}: {elapsed_time:.4f}")
-        print(f"Throughput (in TFLOP/s) for {m}x{n}x{k}, b={b}: "
-          f"{(2 * b * m * n * k) / (elapsed_time * 10**12):.3f}") 
-    print("-" * 80)
+from utils import benchmark_mm
 
 if __name__ == '__main__':
-    torch.cuda.set_device("cuda:0")
+    parser = argparse.ArgumentParser()
+    m_group = parser.add_mutually_exclusive_group(required=True)
+    m_group.add_argument("-m", nargs="+", type=int, help='The first dimension of the GEMM, enter any number of arguments')
+    m_group.add_argument("--m_range", nargs='+', type=int, help="The first dimension of the GEMM, [start,stop,step]")
 
-    arch = "a100"
+    n_group = parser.add_mutually_exclusive_group(required=True)
+    n_group.add_argument("-n", nargs="*", type=int, help='The shared dimension of the GEMM, enter any number of arguments')
+    n_group.add_argument("--n_range", nargs='+', type=int, help="The shared dimension of the GEMM, [start,stop,step]")
 
-    # GEMM m sweep
-    with open(f'../results/gemm_data/mm/m_sweep/{arch}.out', 'w') as sys.stdout:
-        for log_size in range(5, 14):
-            benchmark_mm(2**log_size, 4096, 2**log_size)
+    k_group = parser.add_mutually_exclusive_group(required=True)
+    k_group.add_argument("-k", nargs="*", type=int, help='The last dimension of the GEMM, enter any number of arguments')
+    k_group.add_argument("--k_range", nargs='+', type=int, help="The last dimension of the GEMM, [start,stop,step]")
 
-    # GEMM k sweep low
-    with open(f'../results/gemm_data/mm/ksweep_low/{arch}.out', 'w') as sys.stdout:
-        for k in range(64, 512, 2):
-            benchmark_mm(27648, 4096, k)
+    parser.add_argument("--num_iterations", type=int, default=200, help='The number of iterations used to benchmark each GEMM')
+    parser.add_argument("--num_warmup_iterations", type=int, default=50, help='The number of warmup iterations')
+    parser.add_argument("--cuda_device", type=int, default=0, help="The cuda device to run the benchmark on")
+    parser.add_argument("--output_file", type=str, default="../results/gemm_data/mm.out")
+    args = parser.parse_args()
 
-    #  GEMM k sweep high
-    with open(f'../results/gemm_data/mm/ksweep_high/{arch}.out', 'w') as sys.stdout:
-        for k in range(1536, 6208, 64):
-            benchmark_mm(2304, 4096, k)
+    m = args.m
+    n = args.n
+    k = args.k
+
+    if m is None:
+        start,stop,step = args.m_range
+        m = np.arange(start,stop,step)
+    if n is None:
+        start,stop,step = args.n_range
+        n = np.arange(start,stop,step)
+    if k is None:
+        start,stop,step = args.k_range
+        k = np.arange(start,stop,step)
+    
+    # set cuda device
+    torch.cuda.set_device(f"cuda:{args.cuda_device}")
+
+    # loop through all sizes to benchmark
+    with open(args.output_file, 'w') as sys.stdout:
+        for M in m:
+            for N in n:
+                for K in k:
+                    benchmark_mm(M,N,K, args.num_iterations, args.num_warmup_iterations)
