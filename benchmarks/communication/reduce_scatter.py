646253b00334d3dd9a063b3bb92d9de1c09738ca
Quentin Anthony <qganthony@yahoo.com>
2025-01-22 17:52:53 -0800

    completed the previous commit

--- benchmarks/communication/reduce_scatter.py
+++ benchmarks/communication/reduce_scatter.py
@@ -21,7 +21,15 @@ def timed_reduce_scatter(input, start_event, end_event, args):
     sync_all()
     # Warmups, establish connections, etc.
     for i in range(args.warmups):
-        dist.reduce_scatter_tensor(output, input, async_op=args.async_op)
+        if hasattr(torch.distributed, "reduce_scatter_tensor"):
+            dist.reduce_scatter_tensor(output, input, async_op=args.async_op)
+        elif hasattr(torch.distributed, "_reduce_scatter_base"):
+            dist._reduce_scatter_base(output, input, async_op=args.async_op)
+        else:
+            input_tensors = list(
+                torch.chunk(input,
+                            dist.get_world_size()))
+            dist.reduce_scatter(output, input_tensors, async_op=args.async_op)
     sync_all()
 
     # time the actual comm op trials times and average it
